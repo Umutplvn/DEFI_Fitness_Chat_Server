4187162a45ade8b0907f5728947b78d21742c3da
umutplvn <umutpehlivan2078@gmail.com>
2024-07-27 07:16:58 +0100

    UP

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// Backend kodu
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - DEFI Project
+------------------------------------------------------- */
 
-// Message model
 const MessageSchema = new mongoose.Schema({
   senderId: String,
   receiverId: String,
@@ -8,12 +10,12 @@ const MessageSchema = new mongoose.Schema({
   image: String,
   video: String,
   timestamp: { type: Date, default: Date.now },
-  read: { type: Boolean, default: false } // Mesajın okunup okunmadığı bilgisini ekleyin
+  read: { type: Boolean, default: false }
 });
 
 const Message = mongoose.model('Message', MessageSchema);
 
-// Mesaj gönderme
+// SEND A MESSAGE
 app.post('/api/messages', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'video', maxCount: 1 }]), async (req, res) => {
   const { senderId, receiverId, message } = req.body;
   const image = req.files['image'] ? req.files['image'][0].filename : null;
@@ -28,7 +30,7 @@ app.post('/api/messages', upload.fields([{ name: 'image', maxCount: 1 }, { name:
   res.send(newMessage);
 });
 
-// Kullanıcının son mesajlarını ve okundu bilgilerini almak için API
+// RECEIVE MESSAGE AND ONLINE/OFFLINE INFO
 app.get('/api/chats/:userId', async (req, res) => {
   const { userId } = req.params;
   const messages = await Message.find({
@@ -38,7 +40,7 @@ app.get('/api/chats/:userId', async (req, res) => {
     ]
   }).sort({ timestamp: -1 });
 
-  // Her kullanıcı için son mesajı ve okunma bilgisini bulma
+  // LAST MESSAGES
   const chats = messages.reduce((acc, message) => {
     const otherUserId = message.senderId === userId ? message.receiverId : message.senderId;
     if (!acc[otherUserId]) {
