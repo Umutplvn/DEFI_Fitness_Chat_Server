4b28ad4d3b79c5d99b91851545aecde00de994f2
umutplvn <umutpehlivan2078@gmail.com>
2024-07-27 23:32:05 +0100

    up

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ const io = socketIo(server, {
 // Setup multer for file upload
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/');
+    cb(null, path.join(__dirname, './uploads/')); // Yükleme dizini
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname)); // Dosya adı
   }
 });
 const upload = multer({ storage: storage });
