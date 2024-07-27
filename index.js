a7408efc2efba3190345e096b629fa9ac7941f28
umutplvn <umutpehlivan2078@gmail.com>
2024-07-27 21:37:41 +0100

    up

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT']
   }
 });
